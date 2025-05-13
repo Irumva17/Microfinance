b29bc004d2351e7c34663c39854a6e8714020075
Irumva Arsene <irumvaarsene1@gmail.com>
2025-05-13 10:43:57 +0200

    third commit

--- tenant/admin.py
+++ tenant/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django_tenants.admin import TenantAdminMixin
+from .models import MicrofinanceTenant
 
-# Register your models here.
+@admin.register(MicrofinanceTenant)
+class MicrofinanceTenantAdmin(TenantAdminMixin, admin.ModelAdmin):
+    list_display = ('nom', 'adresse', 'nif')
+    search_fields = ('nom', 'adresse', 'nif')
+    list_filter = ('nom',)
+    ordering = ('nom',)
--- tenant/models.py
+++ tenant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django_tenants.models import TenantMixin, DomainMixin
 
-# Create your models here.
+class MicrofinanceTenant(TenantMixin):
+    nom = models.CharField(max_length=100, unique=True)
+    adresse = models.CharField(max_length=255, null=True, blank=True)
+    nif = models.CharField(max_length=64, unique=True)
+
+    auto_create_schema = True  # important pour créer le schéma automatiquement
+
+    def __str__(self):
+        return self.nom
+    
+class Domain(DomainMixin):
+    
+    pass
